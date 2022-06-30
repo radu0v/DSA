7332b54ab684bc6cf26bc9948fb55f7ab6572997
radu <aradu96.v@gmail.com>
2022-07-01 00:27:35 +0300

    -

--- DS/doubleLinkedList/doubleLinkedList.go
+++ DS/doubleLinkedList/doubleLinkedList.go
@@ -1,35 +1,36 @@
 package doubleLinkedList
 
 import "fmt"
+
 // this is a double linked list implementation
 //the list can be traversed both ways
 
 //create the node. A pointer to a previous node, a pointer to the next node and the data
 
-type Node struct{
+type Node struct {
 	prev *Node
 	data interface{}
 	next *Node
 }
 
-// create the list. 
-type List struct{
+// create the list.
+type List struct {
 	size int
-	head *Node	//pointer to first node
+	head *Node //pointer to first node
 	tail *Node //pointer to last node
 }
 
 //------functions---------------------------------
 
 // insert data into list at the last place
-func (l *List) Insert(d interface{}){
-	node := &Node(prev:nil,data:d,next:nil)
+func (l *List) Insert(d interface{}) {
+	node := &Node{prev: nil, data: d, next: nil}
 	if l.head == nil {
-		l.head == node
-		l.tail == node
-	}else{
-		current = l.head
-		for current.next != nil{
+		l.head = node
+		l.tail = node
+	} else {
+		current := l.head
+		for current.next != nil {
 			current = current.next
 		}
 		node.prev = current
@@ -39,38 +40,32 @@ func (l *List) Insert(d interface{}){
 	l.size += 1
 }
 
-func Show(l *List){
-	if l.head == nil{
+func Show(l *List) {
+	if l.head == nil {
 		fmt.Println("Empty list")
-	}else{
+	} else {
 		current := l.head
-		for current != nil{
-			fmt.Printf("%v--->",current.data)
+		for current != nil {
+			fmt.Printf("%v--->", current.data)
 			current = current.next
 		}
 		fmt.Println("nil")
 	}
 }
 
-func SizeOf(l *List) int{
+func SizeOf(l *List) int {
 	return l.size
 }
 
-func showReverse(l *List){
-	if l.head == nil{
+func showReverse(l *List) {
+	if l.head == nil {
 		fmt.Println("Empty list")
-	}else{
+	} else {
 		current := l.tail
-		for current != nil{
-			fmt.Printf("%v--->",current.data)
+		for current != nil {
+			fmt.Printf("%v--->", current.data)
 			current = current.prev
 		}
 		fmt.Println("nil")
 	}
 }
-
-
-
-
-
-
